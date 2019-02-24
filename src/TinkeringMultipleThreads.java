4f290bf0a5738bb301f17493a10ec0f8e35a04c9
jamesgov <blindmice1o3@gmail.com>
2019-02-23 16:49:21 -0800

    tinker: Practicing the 2 basic ways of spawning off a new thread (extending the Thread class or implementing the Runnable interface to pass into a Thread object's constructor, both of which will require the reference variable to call the start() method [though both overrides the run() method]). Also created an anonymous instance of the Runnable interface to pass into a Thread constructor.

---
+++ src/TinkeringMultipleThreads.java
@@ -0,0 +1,58 @@
+
+class ExtendThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": ExtendThread");
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class RunnableInterface implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": RunnableInterface");
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class TinkeringMultipleThreads {
+
+    public static void main(String[] args) {
+        ExtendThread extendThread = new ExtendThread();
+        extendThread.start();
+
+        Thread runnableInterface = new Thread(new RunnableInterface());
+        runnableInterface.start();
+
+        Thread anonymousRunnableInterface = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(i + ": anonymousRunnableInterface");
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        anonymousRunnableInterface.start();
+    }
+
+}
