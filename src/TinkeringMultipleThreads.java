b0a19e602e79a29cc20fd3b28a7275b112b114ef
jamesgov <blindmice1o3@gmail.com>
2019-02-25 09:39:40 -0800

    tinker: Exploring IntelliJ IDE (split mode for tool windows [project and structure], move to right). Moved inner-classes of TinkeringMultipleThreads to bottom.

--- src/TinkeringMultipleThreads.java
+++ src/TinkeringMultipleThreads.java
@@ -1,3 +1,30 @@
+public class TinkeringMultipleThreads {
+
+    public static void main(String[] args) {
+        ExtendThread extendThread = new ExtendThread();
+        extendThread.start();
+
+        Thread runnableInterface = new Thread(new RunnableInterface());
+        runnableInterface.start();
+
+        Thread anonymousRunnableInterface = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(i + ": anonymousRunnableInterface");
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        anonymousRunnableInterface.start();
+    }
+
+}
 
 class ExtendThread extends Thread {
     @Override
@@ -27,32 +54,4 @@ class RunnableInterface implements Runnable {
             }
         }
     }
-}
-
-public class TinkeringMultipleThreads {
-
-    public static void main(String[] args) {
-        ExtendThread extendThread = new ExtendThread();
-        extendThread.start();
-
-        Thread runnableInterface = new Thread(new RunnableInterface());
-        runnableInterface.start();
-
-        Thread anonymousRunnableInterface = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    System.out.println(i + ": anonymousRunnableInterface");
-
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        anonymousRunnableInterface.start();
-    }
-
 }
