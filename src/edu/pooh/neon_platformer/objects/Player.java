a946912b399a694a22dbe3feb09f49601c8ff5c4
jamesgov <blindmice1o3@gmail.com>
2019-03-08 13:58:38 -0800

    youtube's RealTutsGML - Java Game Programming 7: Collision Bounds. Set up the 4 bounding boxes of the Player class by creating 3 new methods (and adjusting one existing method named getBounds()) that instantiates a new Rectangle of the correctly positioned bounding Rectangles (bottom, top, left, and right).

--- src/edu/pooh/neon_platformer/objects/Player.java
+++ src/edu/pooh/neon_platformer/objects/Player.java
@@ -8,8 +8,8 @@ import java.util.LinkedList;
 
 public class Player extends GameObject {
 
-    private float width = 32;
-    private float height = 64;
+    private float width = 48;
+    private float height = 96;
 
     private float gravity = 0.5f;
     private final float MAX_SPEED = 10;
@@ -21,7 +21,7 @@ public class Player extends GameObject {
     @Override
     public void tick(LinkedList<GameObject> object) {
         x += velX;
-        y += velY;
+        //y += velY;
 
         if (falling || jumping) {
             velY += gravity;
@@ -39,11 +39,30 @@ public class Player extends GameObject {
     public void render(Graphics g) {
         g.setColor(Color.BLUE);
         g.fillRect((int)x, (int)y, (int)width, (int)height);
+
+        Graphics2D g2d = (Graphics2D)g;
+        g.setColor(Color.RED);
+        g2d.draw(getBounds());
+        g2d.draw(getBoundsRight());
+        g2d.draw(getBoundsLeft());
+        g2d.draw(getBoundsTop());
     }
 
     @Override
     public Rectangle getBounds() {
-        return new Rectangle((int)x, (int)y, (int)width, (int)height);
+        return new Rectangle((int)(x+(width/4)), (int)(y+(height/2)), (int)width/2, (int)height/2);
+    }
+
+    public Rectangle getBoundsTop() {
+        return new Rectangle((int)(x+(width/4)), (int)y, (int)width/2, (int)height/2);
+    }
+
+    public Rectangle getBoundsRight() {
+        return new Rectangle((int)(x+(width-5)), (int)y+5, (int)5, (int)height-10);
+    }
+
+    public Rectangle getBoundsLeft() {
+        return new Rectangle((int)x, (int)y+5, (int)5, (int)height-10);
     }
 
 } // **** end Player class ****
