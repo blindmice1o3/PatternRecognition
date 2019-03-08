f2dd2cf7f3e6cf7dd598b6f4e74ecb63dc787db0
jamesgov <blindmice1o3@gmail.com>
2019-03-07 20:42:16 -0800

    youtube's RealTutsGML - Java Game Programming 2: Game Loop. Game class implements the Runnable interface. It's run() method replaced a System.out.println() call with a game loop that I've seen twice before, but still do not fully understand (RealTutsGML claims to not know it well enough to explain too), we're just typing it out and going with it, knowing that the tick() method will update game mechanics/model and the render() method will be where images will be drawn to the screen. Inside the Game class's render() method, we get a BufferStrategy from Canvas class's getBufferStrategy(), check if it's null (the first time we're calling render(), and if it is null, we call Canvas's createBufferStrategy(int) method and pass in a 3 for having 3 sets of BufferedImage ready load and be moved to the screen. We then get the BufferStrategy's Graphics context g and make the background of the canvas a filled black rectangle.

--- src/edu/pooh/neon_platformer/window/Game.java
+++ src/edu/pooh/neon_platformer/window/Game.java
@@ -1,6 +1,7 @@
 package edu.pooh.neon_platformer.window;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 
@@ -19,7 +20,54 @@ public class Game extends Canvas implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Thread has begun.");
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                tick();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames + "   TICKS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
+    }
+
+    private void tick() {
+
+    }
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        ///////////////////////////////////////////////
+        // Draw here.
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        ///////////////////////////////////////////////
+        g.dispose();
+        bs.show();
+
     }
 
     public static void main(String[] args) {
