38d49084d662f327940a879eb1bc0a9f5dad6484
jamesgov <blindmice1o3@gmail.com>
2019-08-11 13:52:01 -0700

    Merge and resolve conflicts

--- src/edu/pooh/evo_side_scroller/Assets.java
+++ src/edu/pooh/evo_side_scroller/Assets.java
@@ -6,9 +6,15 @@ import java.awt.image.BufferedImage;
  * Created by jgov on 7/22/2019.
  */
 public class Assets {
+
     public static BufferedImage mapAndChapterSpriteSheet;
+
+    public static BufferedImage chapter1Intro, chapter2Intro, chapter3Intro, chapter4Intro, chapter5Intro;
 
     public static void initAssets() {
         mapAndChapterSpriteSheet = Utils.loadImage("/sprite sheet - EVO Search for Eden/SNES - EVO Search for Eden - Maps & Chapter Images.png");
+
+        chapter1Intro = mapAndChapterSpriteSheet.getSubimage(146, 6, 255, 221);
+        chapter2Intro = mapAndChapterSpriteSheet.getSubimage(146, 232, 255, 221);
     }
 }
